7b726a711f9a3e0203f1fe858a027d9581abecdc
Binay Shaw <binaysha7777@gmail.com>
2022-07-12 21:34:47 +0200

    Reverse Words in a String (Updated)

--- src/ReverseWordsInAString.java
+++ src/ReverseWordsInAString.java
@@ -1,6 +1,7 @@
 //Leetcode: https://leetcode.com/problems/reverse-words-in-a-string/
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class ReverseWordsInAString {
     public static void main(String[] args) {
@@ -12,10 +13,32 @@ public class ReverseWordsInAString {
     }
 
     private static String reverseWord(String st) {
+        /** Does works on strings like "GAMING    IS MY   LIFE"   **/
+//        StringBuilder sb = new StringBuilder();
+//        String[] s = st.split("\\s");
+//        for (int i=s.length-1; i>=0; i--) {
+//            sb.append(s[i]+" ");
+//        }
+//        return sb.toString().trim();
+
+        /** Works on all string **/
+        Stack<String> stack = new Stack<>();
+        String x = st.trim();
         StringBuilder sb = new StringBuilder();
-        String[] s = st.split("\\s");
-        for (int i=s.length-1; i>=0; i--) {
-            sb.append(s[i]+" ");
+        for(int i=0; i<x.length(); i++) {
+            if(x.charAt(i) == ' ') {
+                if(sb.length()!=0) {
+                    stack.push(sb.toString());
+                    sb.setLength(0);
+                }
+            } else {
+                sb.append(x.charAt(i));
+            }
+        }
+        stack.push(sb.toString());
+        sb.setLength(0);
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop() + " ");
         }
         return sb.toString().trim();
     }
