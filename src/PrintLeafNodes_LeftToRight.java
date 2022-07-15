c230fe96e141cf1e08c52d9694f4540088400b56
Binay Shaw <binaysha7777@gmail.com>
2022-07-15 11:36:02 +0530

    Print Leaf Nodes

---
+++ src/PrintLeafNodes_LeftToRight.java
@@ -0,0 +1,38 @@
+public class PrintLeafNodes_LeftToRight {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode (int data) {
+            this.val = data;
+            this.left = this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(1);
+        root.right.right = new TreeNode(9);
+        root.right.left = new TreeNode(7);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(8);
+
+        printLeftToRight(root);     //Pre-Order principle
+
+        /*       5
+         *      / \
+         *     3   1
+         *    / \  / \
+         *   4  8  7  9
+         *
+         * */
+    }
+
+    private static void printLeftToRight(TreeNode root) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) System.out.print(root.val + " ");
+        printLeftToRight(root.left);
+        printLeftToRight(root.right);
+    }
+}
