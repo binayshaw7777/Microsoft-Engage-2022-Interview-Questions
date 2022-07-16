9f50b120be14b8b04a703718c4d230ed0af8921a
Binay Shaw <binayshaw7777@gmail.com>
2022-07-17 02:44:22 +0530

    checkPalindrome (+2 Approach)

--- src/String/checkPalindrome.java
+++ src/String/checkPalindrome.java
@@ -1,13 +1,39 @@
 package String;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class checkPalindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your String: ");  //Try inputs: "RACECAR", "NOON", "BLOB", "MASON"
         String st = sc.nextLine();
+
+        System.out.println(isPalindromeUsingStack(st));
+        System.out.println(isPalindromeReverseString(st));
         System.out.println(isPalindrome(st));
+    }
+
+    private static boolean isPalindromeReverseString(String st) {
+        StringBuilder sb = new StringBuilder(st);
+        sb.reverse();
+        for (int i=0; i<st.length(); i++) {
+            if (st.charAt(i) != sb.charAt(i))
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean isPalindromeUsingStack(String st) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : st.toCharArray())
+            stack.add(c);
+
+        for (char c : st.toCharArray()) {
+            if (stack.pop() != c)
+                return false;
+        }
+        return true;
     }
 
     private static boolean isPalindrome(String st) {
