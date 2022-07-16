07d1025324f17a6a20accd45b46bac18c9c4947a
Binay Shaw <binayshaw7777@gmail.com>
2022-07-16 09:50:31 +0530

    Anagram String (update)

--- src/String/Anagram.java
+++ src/String/Anagram.java
@@ -3,9 +3,7 @@ String.Anagram means the frequency of every character in both String must be equ
 Leetcode: https://leetcode.com/problems/valid-anagram/
  */
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class Anagram {
     public static void main(String[] args) {
@@ -21,14 +19,33 @@ public class Anagram {
     }
 
     private static boolean setIsAnagram(String a, String b) {
-        if (a.length() != b.length()) return false;
-        HashSet<Character> set = new HashSet<>();
-        for (char c : a.toCharArray())
-            set.add(c);
-
-        for (char c : b.toCharArray()) {
-            if (!set.contains(c)) return false;
+        if (a.length() != b.length()) {
+            return false;
         }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < a.length(); i++) {
+
+            if (map.containsKey(a.charAt(i)))
+                map.put(a.charAt(i), map.get(a.charAt(i)) + 1);
+            else
+                map.put(a.charAt(i), 1);
+
+        }
+
+        for (int i = 0; i < b.length(); i++) {
+            if (map.containsKey(b.charAt(i))) {
+                map.put(b.charAt(i), map.get(b.charAt(i)) - 1);
+            }
+        }
+
+        Set<Character> keys = map.keySet();
+        for (Character key : keys) {
+            if (map.get(key) != 0) {
+                return false;
+            }
+        }
+
         return true;
     }
 
